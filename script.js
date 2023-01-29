1df27a4314d727f2c95d78da21f1d3dded52cf6d
Rocio Anahi <info.rociormz@gmail.com>
2023-01-29 06:43:14 -0300

    curso terminado

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-( () => {
-    
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
@@ -14,35 +15,13 @@ const createTask = (evento) => {
     const titleTask = document.createElement("span");
     titleTask.classList.add("task");
     titleTask.innerText = value;
-    const deleteTask = document.createElement("i");
-    deleteTask.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
     taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    taskContent.appendChild(deleteTask);
     
-    const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
   //task.innerHTML = content;
   task.appendChild(taskContent);
-
+  task.appendChild(deleteIcon());
   list.appendChild(task);
 };
 
 btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-    const i = document.createElement("i");
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener("click", completeTask);
-
-    return i;
-};
-
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("completeIcon");
-    element.classList.toggle("far");
-};
-
-})();
