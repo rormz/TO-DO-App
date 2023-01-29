b7de2f81a092420276ddc752749db5cfe8aab6c3
Rocio Anahi <info.rociormz@gmail.com>
2023-01-29 04:17:22 -0300

    clase03 terminada

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const task = document.createElement("li"); /* creamos el elemento li para luego añadirlo como hijo a ul con appendChild*/
+    task.classList.add("card"); /* le añadimos la clase que tenía antes a través del método classList.add */
+    const list = document.querySelector("[data-list]");
+    input.value = "";
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+  task.innerHTML = content;
+
+  list.appendChild(task);
+
+    console.log(content);
 };
 
 btn.addEventListener("click", createTask);
