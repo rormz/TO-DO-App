0b1431690614301d485a8e3f92aa170a82769b43
Rocio Anahi <info.rociormz@gmail.com>
2023-02-03 15:44:47 -0300

    adding date function

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ import deleteIcon from "./components/deleteIcon.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
+const addTask = (evento) => {
+  const list = document.querySelector("[data-list]");
+  const task = createTask(evento);
+  list.appendChild(task);
+
+};
+
 const createTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
+    const calendar = document.querySelector("[data-form-date]");
     const value = input.value;
+    const date = calendar.value;
+    const dateFormat = moment(date).format("DD/MM/YYYY");
+
     const task = document.createElement("li"); /* creamos el elemento li para luego añadirlo como hijo a ul con appendChild*/
     task.classList.add("card"); /* le añadimos la clase que tenía antes a través del método classList.add */
-    const list = document.querySelector("[data-list]");
+
     input.value = "";
     const taskContent = document.createElement("div");
     const titleTask = document.createElement("span");
@@ -19,9 +30,12 @@ const createTask = (evento) => {
     taskContent.appendChild(titleTask);
     
   //task.innerHTML = content;
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dateFormat;
   task.appendChild(taskContent);
+  task.appendChild(dateElement);
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 };
 
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
