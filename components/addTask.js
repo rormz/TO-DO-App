c5ba63a24991c37e51fce8d8b57a935906035248
Rocio Anahi <info.rociormz@gmail.com>
2023-02-04 03:03:09 -0300

    added new date functions and local storage

---
+++ components/addTask.js
@@ -0,0 +1,71 @@
+import checkComplete from "./checkComplete.js";
+import deleteIcon from "./deleteIcon.js";
+import { displayTasks } from "./readTasks.js";
+
+export const addTask = (evento) => {
+  evento.preventDefault();
+  const list = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input]");
+  const calendar = document.querySelector("[data-form-date]");
+
+  const value = input.value;
+  const date = calendar.value;
+  const dateFormat = moment(date).format("DD/MM/YYYY");
+
+  if (value === "" || date === "") {
+    return;
+  }
+
+  input.value = "";
+  calendar.value = "";
+
+  const complete = false;
+
+  const taskObj = {
+    value,
+    dateFormat,
+    complete,
+    id: uuid.v4(),
+  };
+
+  list.innerHTML = "";
+
+  const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+  taskList.push(taskObj);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+
+  displayTasks();
+};
+
+export const createTask = ({ value, dateFormat, complete, id }) => {
+  const task =
+    document.createElement(
+      "li"
+    ); /* creamos el elemento li para luego añadirlo como hijo a ul con appendChild*/
+  task.classList.add(
+    "card"
+  ); /* le añadimos la clase que tenía antes a través del método classList.add */
+  const taskContent = document.createElement("div");
+
+  const check = checkComplete(id);
+
+  if (complete) {
+    check.classList.toggle("fas");
+    check.classList.toggle("completeIcon");
+    check.classList.toggle("far");
+  }
+
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value;
+  taskContent.appendChild(check);
+  taskContent.appendChild(titleTask);
+
+  //task.innerHTML = content;
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dateFormat;
+  task.appendChild(taskContent);
+  task.appendChild(dateElement);
+  task.appendChild(deleteIcon(id));
+  return task;
+};
--- script.js
+++ script.js
@@ -1,41 +1,8 @@
-import checkComplete from "./components/checkComplete.js";
-import deleteIcon from "./components/deleteIcon.js";
+import { addTask } from "./components/addTask.js";
+import { displayTasks } from "./components/readTasks.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
-const addTask = (evento) => {
-  const list = document.querySelector("[data-list]");
-  const task = createTask(evento);
-  list.appendChild(task);
-
-};
-
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const calendar = document.querySelector("[data-form-date]");
-    const value = input.value;
-    const date = calendar.value;
-    const dateFormat = moment(date).format("DD/MM/YYYY");
-
-    const task = document.createElement("li"); /* creamos el elemento li para luego añadirlo como hijo a ul con appendChild*/
-    task.classList.add("card"); /* le añadimos la clase que tenía antes a través del método classList.add */
-
-    input.value = "";
-    const taskContent = document.createElement("div");
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    
-  //task.innerHTML = content;
-  const dateElement = document.createElement("span");
-  dateElement.innerHTML = dateFormat;
-  task.appendChild(taskContent);
-  task.appendChild(dateElement);
-  task.appendChild(deleteIcon());
-  return task;
-};
-
 btn.addEventListener("click", addTask);
+
+displayTasks();
